5f30acf38cf5936073e90bf335c323fc9e2f8774
skvoren <zayonvx@gmail.com>
2022-11-27 13:14:21 +0400

    exercise 11.9

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const process = require('process')
+const { useApi } = require('./src/useApi')
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
